c78f3a8bf3a0148feb05b3646e9f09248a5d7a9f
Wasan Udomtanasub <poul2545@gmail.com>
2024-02-03 13:21:11 +0700

    daft function

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// interface merge (int[] collection_1, int[] collection_2, int[] collection_3) : int []
+
+export function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[] {
+    return [0,1]
+}
